2ab93cd08df59bc8bce59b19a0acd98caa43b467
tsixlas <tsixlas@gmail.com>
2024-10-13 18:15:23 +0100

    Add template example

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,39 @@ void PrettyFoo(MyClass::Ptr p);
 // Templates //
 using Lookup = std::map<std::string, MyClass::Ptr>;
 
+// Macros //
+// The definition 
+#define max(a, b) a > b ? a : b 
+
+void usage_of_macros(void){
+    float value1 = 2.7; 
+    float value2 = 3.4; 
+    float result = max(value1, value2); 
+    // The above line is the same as:
+    float result1 = value1 > value2 ? value1 : value2; 
+
+    // Correct final semicolon (not) use on the definition allows more natural use
+    float result2 = max(value1, value2) + 0.4; 
+}
+
+#define FIELD(TYPE, NAME)      \
+public:                        \
+    void Set##NAME(TYPE v){    \
+        m_##NAME = v;          \
+    }                          \
+    TYPE Get##NAME(){          \
+        return m_##NAME;       \
+    };                         \
+private:                       \
+    TYPE m_##NAME
+
+//Seriously simplifies the definition of data classes
+class DataClass{
+    FIELD(int, One);
+    FIELD(float, Two);
+};
+
+
 int main(void){
     cout << "Hallo world" << endl;
 }
