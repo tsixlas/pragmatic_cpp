cfc2492440873b318efc9583e3837b4708ab9f4c
tsixlas <tsixlas@gmail.com>
2025-05-18 20:52:22 +0100

    Add CreateMacro

---
+++ CreateMacro/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define CREATE( NAME )              \
+void My##NAME(){                    \
+    std::cout << #NAME << std::endl;\
+}                                   \
+
+namespace {
+    CREATE(Fred);
+}
+
+int main() {
+    MyFred();
+}
