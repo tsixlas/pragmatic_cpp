40e8d3ef6729de7b8115c018309a7c5d1225e491
tsixlas <tsixlas@gmail.com>
2025-05-18 21:04:05 +0100

    Add FieldMarco

---
+++ FieldMarco/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define FIELD( TYPE, NAME )             \
+    public:                             \
+        void Set##NAME( const TYPE v ){ \
+            m_##NAME = v;               \
+        }                               \
+        TYPE const Get##NAME() {        \
+            return m_##NAME;            \
+        }                               \
+    private:                            \
+        TYPE m_##NAME{0}                \
+
+class MyFieldClass {
+    FIELD(int, One);
+    FIELD(float, Two);
+};
+
+class MyFieldClassEquivalent {
+public:
+    void SetOne(const int v) { m_One = v; }
+    int GetOne() const { return m_One; }
+    void SetTwo(const float v) { m_Two = v; }
+    float GetTwo() const { return m_Two; }
+
+private:
+    int m_One{0};
+    float m_Two{0};
+};
+
+int main() {
+}
